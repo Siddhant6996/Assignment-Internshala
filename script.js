ae8c4ef365567d69bb1ec1f90def00af798b56f0
Siddhant6996 <104733697+Siddhant6996@users.noreply.github.com>
2023-05-21 01:58:34 +0530

    uploaded script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function validateForm() {
+    var input = document.createElement("input");
+    input.type = "text";
+    input.placeholder = "Enter your name";
+    document.getElementById("dynamicContent").appendChild(input);
+  }
+  
